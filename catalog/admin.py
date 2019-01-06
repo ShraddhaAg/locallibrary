e40272fca9f0fdabf62b76398185aead9b487cb2
ShraddhaAg <shraddha.agrawal000@gmsil.com>
2019-01-06 15:43:41 +0530

    Add dependencies for home page

--- catalog/admin.py
+++ catalog/admin.py
@@ -13,12 +13,11 @@ class BooksInline(admin.TabularInline):
     model = Book
     extra = 0
 
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
     inlines = [BooksInline]
-
-admin.site.register(Author, AuthorAdmin)
 
 class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
