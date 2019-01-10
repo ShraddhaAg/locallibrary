cc57205d599ddc20fc0eec600ed4262e06b5b224
ShraddhaAg <shraddha.agrawal000@gmsil.com>
2019-01-10 22:37:15 +0530

    Add Book List and Detail Views

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from django.urls import reverse
 
 class Genre(models.Model):
 
--- catalog/urls.py
+++ catalog/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path(r'^books/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path(r'^authors/(?P<pk>\d+)$', views.AuthDetailView.as_view(), name='author-detail'),
 ]
